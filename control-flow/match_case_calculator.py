5317acee4d31cd8a23184719d0d57b0bc3f27a77
Charles Agoe <agoetee@gmail.com>
2024-06-27 14:55:51 -0700

    transfering from ubuntu py -v 3.8

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,1 +1,11 @@
-# This program uses match-case for a multiplication table
+# This program uses match-case for a calculator
+
+#!/usr/bin/env python3
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print("The result is", num1 + num2)
