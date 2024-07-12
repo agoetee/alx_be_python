3ba8d6ba1d2642f2d6aef2aa122448e7140a7de9
Charles Agoe <agoetee@gmail.com>
2024-07-12 11:48:47 +0000

    remove comments for clean code

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,16 +5,13 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        #print(f"Deposited: ${amount}")
         return self.account_balance
     
     def withdraw(self, amount):
         if self.account_balance > amount:
             self.account_balance -= amount
-            #print(f"Withdrew: ${amount}")
             return True
         else:
-            #print("Insufficient funds.")
             return False
         
     def display_balance(self):
