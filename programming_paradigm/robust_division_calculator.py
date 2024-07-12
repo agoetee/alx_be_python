6bc7a2e5eb614bf303402c0b0d7d5c0b4328fd92
Charles Agoe <agoetee@gmail.com>
2024-07-12 12:08:54 +0000

    handles exception in division

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,15 @@
+"""
+This program implements a division calculator that robustly handles errors 
+like division by zero and non-numeric inputs using command line arguments
+"""
+
+def safe_divide(numerator, denominator):
+    try:
+        result = int(numerator) / int(denominator)
+        return result
+    
+    except ZeroDivisionError:
+        return "Cannot divide by Zero"
+
+    except ValueError:
+       return "The value has to be a number"
