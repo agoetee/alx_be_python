7fef737dd9c35ce861fa26aaa9514f4309d549e9
Charles Agoe <agoetee@gmail.com>
2024-07-13 09:44:20 +0000

    change divide to division

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -25,7 +25,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(5, -3), -15)
 
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-15, -5), 3)
         self.assertEqual(self.calc.divide(20, -10), -2)
