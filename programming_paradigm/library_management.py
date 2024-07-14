03b4b6393b1af5434e23c96f133f7927f1923c38
Charles Agoe <agoetee@gmail.com>
2024-07-14 07:46:12 +0000

    add comments to blocks

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -25,33 +25,42 @@ class Book:
 
 class Library:
     def __init__(self):
+        """
+        Private attributes of two lists        
+        """
         self._books = []
         self._checked_out_books = []
         
 
     def add_book(self, book):
-        # Adds a book to the list of books in library
+        """
+        Adds a book to the list of books in library
+        """
         self._books.append(book)
         return
 
     def check_out_book(self, title):
         """
         Loop through the list. If title matches a title,
-        remove it and break from the loop
+        remove _books list and append it to _checked_out_books.
+        Break from the loop and print remaining books in the _books list.
         """
+
         for book in self._books:
             if book.title == title:
                 book._is_checked_out = True
                 self._books.remove(book)
                 self._checked_out_books.append(book)
                 break
-        """
-        Print the remaining books
-        """
         for remain in self._books:
             print(f"{remain.title} by {remain.author}")
 
     def return_book(self, title):
+        """
+        Loop through _checked_out_books list and find a match to the title.
+        with a match, add the book to _books list and remove it from _checked_out_books
+        Then print the updated list
+        """
         for book in self._checked_out_books:
             if book.title == title:
                 book._is_checked_out = False
